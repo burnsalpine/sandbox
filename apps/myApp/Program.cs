7e6cc8a3fdcad575f0205e4bc76719d7ae33330b
burnsalpine <burnsalpine@gmail.com>
2018-06-02 00:12:43 -0700

    Update

---
+++ apps/myApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number:");
+            var number1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Pick another number:");
+            var number2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Adding the two numbers: " + (number1 + number2));
+            //Console.WriteLine("Press any key to exit.");
+            //Console.ReadKey();
+        }
+    }
+}
